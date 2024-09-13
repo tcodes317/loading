f0c6890e0aa06344036b95f73966ffcb273d03ca
tcodes317 <tcodes317@gmail.com>
2024-09-13 12:12:33 +0100

    9/13/2024

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-let bg=document.createElement("div");document.body.appendChild(bg);let lo=document.createElement("div");bg.style="width: 100%; height: 100vh; background-color: #fff";
+// setTimeout(()=>{
+//   location.reload();
+// }, 3000)
+
+
+let bg=document.createElement("div");document.body.appendChild(bg);bg.style="width: 100%;height: 100vh; background-color: #fff;display: flex;align-items:center; justify-content: center";let lo=document.createElement("div");bg.appendChild(lo);lo.style="width: 30px;height: 30px;border-top: 5px solid #000;border-radius: 100px";lo.id="dif"
